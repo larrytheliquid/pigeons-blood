99302006b0b8bf00bfd248f56e7a884314e35f80
Larry Diehl <larry@izea.com>
2008-07-09 01:56:46 -0400

    Renamed proc_to_ruby to proc_contents_string

--- lib/pigeons_blood.rb
+++ lib/pigeons_blood.rb
@@ -3,18 +3,18 @@ require 'ruby2ruby'
 
 module PigeonsBlood
   def is_defined?(&block)
-    eval "defined? #{proc_to_ruby(block)}", block
+    eval "defined? #{proc_contents_string(block)}", block
   end
   
   def class!(name, parent=nil, &block)
     eval %{class #{name}#{" < #{parent}" if parent}
-            #{proc_to_ruby(block)}
+            #{proc_contents_string(block)}
           end}, block
   end
   
 private
 
-  def proc_to_ruby(proc)
+  def proc_contents_string(proc)
     proc.to_ruby.sub(/^proc \{/, '').chomp('}')
   end
 end
